e6c59900344f776209c23c757483365c82bfc212
Bihan Chakraborty <bihan.chakraborty@adpushup.com>
2025-01-01 15:51:58 +0530

    fix commit

--- java-project-part1/src/main/java/com/application/springboot/SpringbootApplication.java
+++ java-project-part1/src/main/java/com/application/springboot/SpringbootApplication.java
@@ -3,18 +3,17 @@ package com.application.springboot;
 import com.application.springboot.entity.Role;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-//@ComponentScan("com.application")
-@ComponentScan(basePackages = {"com.application.springboot", "com.application.sharedlibrary"})
-//@Configuration
-//@EntityScan("com.application")
+@ComponentScan("com.application")
+@EnableJpaRepositories("com.application")
+@EntityScan("com.application")
 public class SpringbootApplication {
 
   public static void main(String[] args) {
--- java-project-part3/src/main/java/com/application/springboot/dao/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.application.springboot.dao;
-
-import com.application.springboot.entity.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Integer> {
-
-  //Optional<User> findByEmail(String email);
-}
