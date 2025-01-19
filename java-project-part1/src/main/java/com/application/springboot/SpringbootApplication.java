62f3a493361eaf091238a93de5f7bc43abd5452b
niharika2k00 <dniharika16@gmail.com>
2025-01-20 00:01:12 +0530

    add default roles once the spring app starts

--- java-project-part1/src/main/java/com/application/springboot/SpringbootApplication.java
+++ java-project-part1/src/main/java/com/application/springboot/SpringbootApplication.java
@@ -1,14 +1,10 @@
 package com.application.springboot;
 
-import com.application.sharedlibrary.entity.Role;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @ComponentScan("com.application")
@@ -17,24 +13,10 @@ import java.util.List;
 public class SpringbootApplication {
 
   public static void main(String[] args) {
+
     System.out.println("Project started 👀...");
-
-    //createRoleTable();
     SpringApplication.run(SpringbootApplication.class, args);
-  }
 
-  // Add all roles in the database at the beginning of the application
-  public static void createRoleTable() {
-    List<Role> roles = new ArrayList<>();
-
-    roles.add(new Role("ROLE_ADMIN"));
-    roles.add(new Role("ROLE_DEVELOPER"));
-    roles.add(new Role("ROLE_EDITOR"));
-    roles.add(new Role("ROLE_GUEST"));
-    roles.add(new Role("ROLE_MANAGER"));
-    roles.add(new Role("ROLE_OPERATOR"));
-    roles.add(new Role("ROLE_SUPER_ADMIN"));
-    roles.add(new Role("ROLE_USER"));
-    roles.add(new Role("ROLE_VIEWER"));
+    // 1. Initialise default roles - RoleInitializer Class
   }
 }
